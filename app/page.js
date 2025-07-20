fffad5cdc6038977def8a362e8b6844510c062be
ArshhMansuri <arshashfaqhuseinmansuri@gmail.com>
2025-07-20 14:33:56 +1000

    updated hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button class="btn">Get Started</button>
+      <h1>
+        Collect customer feedback to build better products,priortise features{" "}
+      </h1>
     </main>
   );
 }
